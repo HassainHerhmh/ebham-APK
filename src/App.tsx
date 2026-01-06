aea8c518d21f797855da82882502ede43823aebd
HassainHerhmh <aabcaade6@gmail.com>
2026-01-06 21:29:08 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40, textAlign: "center" }}>
+      <h1>🚀 Ebham App</h1>
+      <p>تم تشغيل المشروع بنجاح</p>
+    </div>
+  );
+}
