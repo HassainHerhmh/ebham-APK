ec1837c2f1d7d8826d85192e47d3783d96594bfb
HassainHerhmh <aabcaade6@gmail.com>
2026-01-06 22:27:09 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+
 export default function App() {
   return (
-    <div style={{ padding: 40, textAlign: "center" }}>
-      <h1>🚀 Ebham App</h1>
-      <p>تم تشغيل المشروع بنجاح</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* صفحة تسجيل الدخول */}
+        <Route path="/login" element={<Login />} />
+
+        {/* الصفحة الرئيسية (مؤقتًا) */}
+        <Route
+          path="/"
+          element={
+            <div style={{ textAlign: "center", marginTop: "100px" }}>
+              <h1>Ebham App 🚀</h1>
+              <p>تم تشغيل المشروع بنجاح</p>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
